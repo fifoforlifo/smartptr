e62ff5ef96ad4c52a6f5c32b3a07f875e3f66943
Avinash Baliga <fifoforlifo@gmail.com>
2017-02-21 06:57:15 -0600

    document IntrusivePtr

--- smartptr/IntrusivePtr.h
+++ smartptr/IntrusivePtr.h
@@ -5,6 +5,21 @@
 
 namespace ci0 {
 
+    // IntrusivePtr assignment rules are similar to CComPtr
+    //  *   Assignment from a raw pointer acts like a copy; add_ref() is called.
+    //  *   The constructor performs add_ref() by default, for parity with assignment.
+    //  *   Taking ownership requires calling Attach(), passing addRef=false.
+    //  *   Taking ownership at construction time requires an explicit constructor
+    //      call, passing addRef=false.
+    //
+    // Most new objects have an initial refcount of 1, to avoid the perf overhead
+    // of an initial add_ref() call.  Such objects should never be created like this:
+    //      IntrusivePtr<Foo> pFoo = new Foo();
+    //      pFoo = new Foo();
+    // In both cases, a memory leak will occur since the constructor/operator= will
+    // perform an additional add_ref(), resulting in a single reference with refcount=2.
+
+
     template <class Object>
     class IntrusivePtr
     {
